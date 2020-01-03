4317dcbfcbf7333855fb095f5ea1affeb0087361
vVviewVv <viewissara1246@gmail.com>
2020-01-03 16:17:27 +0700

    HW (done)

--- lab4_3.cpp
+++ lab4_3.cpp
@@ -1,1 +1,22 @@
+#include<iostream>
+using namespace std;
 
+int findDivisor(int y){
+  int x = 2;
+  int ans;
+
+  while(y%x != 0){
+      x = x+1;
+  }
+    ans = x;
+  return ans;
+
+}
+
+int main(){
+  int y;
+  cout << "Enter y =";
+  cin >> y;
+  cout << "Divisor =" << findDivisor(y);
+  return 0;
+}
