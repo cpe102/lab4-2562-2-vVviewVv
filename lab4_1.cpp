e059685a99d86fb804f03c43a7bdddff03c73778
vVviewVv <viewissara1246@gmail.com>
2020-01-03 15:36:20 +0700

    HW (done)

--- lab4_1.cpp
+++ lab4_1.cpp
@@ -1,1 +1,20 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+double findy (int x){
+    double y =(3*pow(x,3))+(2*2.71828)+(pow(2,(2*x)+1))+(sqrt((x*x)+1));
+    return y;
+}
+    
+int main(){
+    
+    int x;
+    cout<< "Enter x =";
+    cin>> x;
+    cout<< "Result y = " << findy (x);
+
+    return 0;
+
+}
 
